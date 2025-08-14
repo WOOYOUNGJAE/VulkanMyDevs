b39a250c2fd9da71f94f47db3e107cf82fd305a9
WOO YOUNGJAE <76108701+WOOYOUNGJAE@users.noreply.github.com>
2025-08-14 16:44:11 +0900

    init skeletalanimiation

---
+++ MyDevs/myRayTracingLittleAdvanced/mySkeletalAnimationRT.h
@@ -0,0 +1,122 @@
+#pragma once
+#include "myRayTracingLittleAdvanced.h"
+
+
+class MySkeletalAnimationRT : public MyVulkanRTBase
+{
+private:
+	VkPhysicalDeviceDescriptorIndexingFeaturesEXT physicalDeviceDescriptorIndexingFeatures{};
+public:	// BLAS
+	struct PerBLASBuildInfo // per blas
+	{
+		VkDeviceSize blasScratchSizeMax = 0;
+		VkDeviceSize asSize;
+		ScratchBuffer blasScratchBuffer{};
+		std::vector<VkAccelerationStructureGeometryKHR> asGeometries{};
+
+		std::vector<VkAccelerationStructureBuildRangeInfoKHR> buildRangeInfos{}; // copy this ptr to ASBuildSets
+		VkAccelerationStructureBuildGeometryInfoKHR asBuildGeometryInfo{}; // copy to ASBuildSets
+	};
+	std::vector<PerBLASBuildInfo> staticPerBlasBuildInfos, dynamicPerBlasBuildInfos;
+	struct ASBuildSets // SOA
+	{
+		std::vector<VkAccelerationStructureBuildGeometryInfoKHR> buildGeometryInfos; // per blas
+		std::vector<VkAccelerationStructureBuildRangeInfoKHR*> buildRangeInfosArray; // double ptr.
+	}staticBlasBuildingSets{}, dynamicBlasBuildingSets{};
+public: // TLAS
+	BufferWithDeviceAddress blasInstancesBuffer; // for tlas
+	VkAccelerationStructureBuildGeometryInfoKHR tlasBuildGeometryInfo{};
+	VkDeviceSize tlasSize;
+	VkAccelerationStructureGeometryKHR tlasGeometry{};
+	VkDeviceSize tlasScratchSize = 0;
+	ScratchBuffer tlasScratchBuffer{};
+	std::vector<VkAccelerationStructureInstanceKHR> blasInstances{};
+	// AS
+	std::vector<AccelerationStructure> staticBLASes, dynamicBLASes;
+	AccelerationStructure TLAS{};
+
+	vks::Buffer vertexBuffer;
+	vks::Buffer indexBuffer;
+	uint32_t indexCount{ 0 };
+	vks::Buffer transformBuffer;
+
+	std::vector<VkRayTracingShaderGroupCreateInfoKHR> shaderGroups{};
+	struct ShaderBindingTables {
+		ShaderBindingTable raygen;
+		ShaderBindingTable miss;
+		ShaderBindingTable hit;
+	} shaderBindingTables;
+
+	vks::Texture2D texture;
+
+	struct UniformData {
+		glm::mat4 viewInverse;
+		glm::mat4 projInverse;
+		uint32_t frame{ 0 };
+	} uniformData;
+	vks::Buffer uniformBuffer;
+
+	struct PushConstantData
+	{
+		uint64_t sceneVertexBufferDeviceAddress = 0;
+		uint64_t sceneIndexBufferDeviceAddress = 0;
+	}pushConstantData;
+
+	// RT Pipeline
+	VkPipeline rtPipeline{ VK_NULL_HANDLE };
+	VkPipelineLayout rtPipelineLayout{ VK_NULL_HANDLE };
+	VkDescriptorSet rtDescriptorSet{ VK_NULL_HANDLE };
+	VkDescriptorSetLayout rtDescriptorSetLayout{ VK_NULL_HANDLE };
+
+	// Compute Pipeline
+	VkPipeline computePipeline{ VK_NULL_HANDLE };
+	VkPipelineLayout computePipelineLayout{ VK_NULL_HANDLE };
+
+	myglTF::ModelRT model;
+
+public:
+	MySkeletalAnimationRT();
+	~MySkeletalAnimationRT() override;
+
+	void createAccelerationStructureBuffer(AccelerationStructure& accelerationStructure, VkAccelerationStructureBuildSizesInfoKHR buildSizeInfo, VkBufferUsageFlagBits usageFlag);
+
+	// Create class buffer and initialize indirect build info, but don't build on GPU yet.
+	void initCLASes();
+	/*
+		Create the bottom level acceleration structure that contains the scene's actual geometry (vertices, triangles)
+	*/
+	// Only Called once after model loaded
+	void initBLASes();
+	void initTLAS();
+
+	void buildBLASes(); // Build or Update
+	void buildTLAS();
+
+	void createShaderBindingTables();
+	void createRayTracingPipeline();
+	void createDescriptorSets();
+	void createUniformBuffer();
+
+	void createComputePipeline();
+	void dispatchClusteredBlasUpdate();
+
+	void handleResize();
+
+	/*
+		Command buffer generation
+	*/
+	void buildCommandBuffers();
+
+	void updateUniformBuffers();
+
+	void getEnabledFeatures();
+
+	void loadAssets();
+
+	void enableExtensions() override;
+	void prepare() override;
+
+	void draw();
+
+	virtual void render();
+};
